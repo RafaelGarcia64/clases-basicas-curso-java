4b54d13ed3613cf6b9c7e89adff9b643986d243f
Rafael Garcia <regestra64@gmail.com>
2018-11-20 11:52:27 -0600

    formato al codigo

--- Promedios.java
+++ Promedios.java
@@ -1,4 +1,4 @@
-public class Promedios{//esta creo que seria nuestra clase abstracta.
+public class Promedios {// esta creo que seria nuestra clase abstracta.
 
 public CalPromedioMateria(){
 	double promedio = 0.0;
@@ -7,11 +7,12 @@ public CalPromedioMateria(){
 	contador += notas[i];	
 	return promedio =(contador/4);}
 	}
-	
-public void PromedioGlobal(){
-	double contador = 0.0;
-	for(int i = 0; i<m; i++){
-		contador+=promedio;
-	}System.out.println("Promedio Global es:" + contador/m);	 
-	}	
-}	
+
+	public void PromedioGlobal() {
+		double contador = 0.0;
+		for (int i = 0; i < m; i++) {
+			contador += promedio;
+		}
+		System.out.println("Promedio Global es:" + contador / m);
+	}
+}

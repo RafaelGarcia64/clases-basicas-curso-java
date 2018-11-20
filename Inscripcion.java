350db34413791fedbb4aedb73cac3a828ea617fe
Rafael Garcia <regestra64@gmail.com>
2018-11-20 11:36:05 -0600

    Cambios menores

---
+++ Inscripcion.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Inscripcion{
+	Scanner in = new Scanner (System.in);
+	Alumno a;
+ ArrayList<Alumno> lista; 
+ int n;
+ public Inscripcion(){
+	 a = new Alumno();
+	 lista = new ArrayList<>();
+ }
+	public Inicializacion(){
+		System.out.println("Ingrese el numero de estudiantes");
+		n = in.nextInt();
+	}	
+		
+	public void DatosAlumno(){
+	 for(int i=0; i<n; i++){
+		// la lista comienza con la posicion 0 hasta n ; pero empiezo con el numero 1 hasta n+1.
+		System.out.println("Ingrese el nombre y el apellido del alumno "+ (i+1));
+		 System.out.println("Nombre");
+		 a.setNombre(in.nextLine());
+		 System.out.println("Apellido");
+		 a.setApellido(in.nextLine());
+		 lista.add(a);
+	 }
+	}
+	
+	public void InscripcionMateria(){
+	//Limite de materias a inscribir 5 
+	int m;
+	System.out.println("¿Cuantas materias desea inscribir?");
+	m = in.nextInt();
+	//mostrar materias que puede inscribir
+	   System.out.println("Estas son las materias que puede inscrbir y su codigo");
+	   System.out.println("1.Mat01 \n2.Pro01 \n3.Fis01 \n4.His01 \n5.Psi01 \n6.Inf01 \n7.Man01 \n8.Qui01 \n9.TecQ01 \n10.Estru01");
+	//seleccionar para guardar las materias que inscribio
+		
+	}	
+	
+	public void DatosCarrera(){
+	//ingresa las materia inscritas y las notas correspondientes a cada materia.
+	//cantidad de notas 4 por materia con un 25% cada una. 
+	//tipo de excepcion en las notas import java.util.InputMismatchException; 
+	
+	}
+	
+	public void imprimir(){
+		for(int i = 0; i<lista.size(); i++){
+		Alumno a = lista.get(i);	
+		System.out.println(" Estudiante "+(i+1));
+		System.out.println("Nombre: "+a.getNombre()+","+a.getApellido());
+		//imprimir tambien el promedio de las materias y el global.
+		}
+	}
+}
+	
+	
---
+++ Promedios.java
@@ -0,0 +1,17 @@
+public class Promedios{//esta creo que seria nuestra clase abstracta.
+
+public CalPromedioMateria(){
+	double promedio = 0.0;
+	double contador = 0.0;
+	for(int i = 0; i<4; i++){
+	contador += notas[i];	
+	return promedio =(contador/4);}
+	}
+	
+public void PromedioGlobal(){
+	double contador = 0.0;
+	for(int i = 0; i<m; i++){
+		contador+=promedio;
+	}System.out.println("Promedio Global es:" + contador/m);	 
+	}	
+}	

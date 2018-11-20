6acfe04b19c479760da91f6a531b3523d45884bf
Rafael Garcia <regestra64@gmail.com>
2018-11-20 14:57:29 -0600

    creacion de metodos propios y correciones de propiedades

--- Materias.java
+++ Materias.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import jdk.nashorn.internal.objects.annotations.Getter;
 
@@ -6,17 +8,19 @@ public class Materias {
 
     private String nombreMateria;
     private String codigoMateria;
-    private List<Double> notas = new ArrayList<>();
+    private List<String> alumnos = new ArrayList<>();
 
     /**
      * Constructor para definir directamente el nombre y codigo de la materia
      * 
      * @param nombreMateria recive un string con el nombre de la materia
      * @param codigoMateria recive un string con el codigo de la materia
+     * @param alumnos recive una lista con los alumnos de dicha materia
      */
-    public Materias(String nombreMateria, String codigoMateria) {
+    public Materias(String nombreMateria, String codigoMateria, List alumnos) {
         this.nombreMateria = nombreMateria;
         this.codigoMateria = codigoMateria;
+        this.alumnos = alumnos;
     }
 
     /**
@@ -27,7 +31,7 @@ public class Materias {
     }
 
     /*
-     *Getters y setters para acceder a las propiedades de el objeto materia
+     * Getters y setters para acceder a las propiedades de el objeto materia
      */
     public String getNombremateria() {
         return this.nombreMateria;
@@ -37,12 +41,30 @@ public class Materias {
         this.nombreMateria = nombreMateria;
     }
 
-    public List<Double> getNotas() {
-        return this.notas;
+    public String getCodigomateria() {
+        return this.codigoMateria;
     }
 
-    public void setNotas(List<Double> notas) {
-        this.notas = notas;
+    public void setCodigomateria(String codigoMateria) {
+        this.codigoMateria = codigoMateria;
     }
 
+    public List<String> getAlumnos() {
+        return this.alumnos;
+    }
+
+    public void setAlumnos(List<String> alumnos) {
+        this.alumnos = alumnos;
+    }
+
+    /*
+     * Metodos propios de el objeto para su funcionabilidad
+     */
+    public void setNuevoAlumno(String alumno) {
+       try {
+        this.alumnos.add(alumno);
+       } catch (Exception e) {
+           System.out.println("Exception: "+e.getMessage());
+       }
+    }
 }
